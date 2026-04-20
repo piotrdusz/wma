5f406e622d4f33553402acb872c1aa12943b810c
Piotr Duszak <piotrdusz@yahoo.pl>
2026-04-20 14:09:13 +0200

    Szablon do pierwszych i drugich zajęć

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,87 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
-using std::cout;
-using std::endl;
+using namespace cv;
 
-int main(int argc, char **argv)
+void ChangeContrast(const Mat &input, int alfa, int beta)
 {
-    cout << "CV_MAJOR = " << CV_MAJOR_VERSION << endl;
-    cout << "CV_MINOR = " << CV_MINOR_VERSION << endl;
-    cout << "============= TEST PASSED =============" << endl;
+    // Zmiana kontrastu tak, aby intensywność każdego piksela była pomnożona przez alfa i dodane beta
+}
+
+void TwoImageOperations(const Mat &input1, const Mat &input2)
+{
+    // Dodaj dwa obrazy
+
+    // Dodaj z wagami
+
+    // Odejmij
+
+    // Różnica absolutna
+
+}
+
+void ApplyThreshold(const Mat &input, int val)
+{
+    // Binaryzacja
+
+    // Różne rodzaje
+}
+
+void MorphologicalOperations(const Mat &input, int kernel_size)
+{
+    // Element strukturalny
+
+    // Dylatacja
+
+    // Erozja
+
+    // Inne
+}
+
+void Filtering(const Mat &input, int kernel_size)
+{
+    // Filtr uśredniający
+
+    // Filtr gaussowski
+
+    // Filtr medianowy
+}
+
+void SimpleMasking(const Mat &input)
+{
+    // Tworzenie czarnego obrazu
+
+    // Narysowanie białych kształtów
+
+    // Zostawienie tylko tych fragmentów obrazu, które na masce są białe
+}
+
+void PixelIteration(const Mat &input)
+{
+    // Iteracja pętlą przez wszystkie piksele i dokonanie jakiejś operacji
+}
+
+void GeometricOperations(const Mat& input)
+{
+    // Translacja
+
+    // Obrót
+
+}
+
+void VideoPlayer()
+{
+    // Wyświetlenie wideo
+}
+
+int main(int argc, char** argv)
+{
+    // Wczytanie obrazu
+
+    // Wyświetlenie obrazu
+
+    // Wywoływanie uzupełnionych funkcji
+
+    return 0;
 }
